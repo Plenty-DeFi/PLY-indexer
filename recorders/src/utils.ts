af6bd1a0518e512f3cffa7c2b00d4d9fa93f14d0
Aniket sindhu <sindhuaniket@gmail.com>
2022-10-21 20:15:36 +0530

    fix: increased starting block

--- recorders/src/utils.ts
+++ recorders/src/utils.ts
@@ -5,7 +5,7 @@ export const addRetryToAxios = () => {
   axios.defaults.timeout = 30000;
   axios.defaults.httpsAgent = new https.Agent({ keepAlive: true });
   axios.interceptors.response.use(null, async (error) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await new Promise((resolve) => setTimeout(resolve, 2000));
     console.log(`
       Axios request error: ${error.message},\n
       URL: ${error.config.url}
